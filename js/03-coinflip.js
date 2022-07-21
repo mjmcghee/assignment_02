498949f139999abfd333f455780af551205bc1a3
Marcus <marcusjm@gmail.com>
2022-07-20 18:36:53 -0700

    corrected the coinflip.js logic

--- js/03-coinflip.js
+++ js/03-coinflip.js
@@ -1,11 +1,11 @@
 function whatSide(coinFlip,choice) {
-    if (coinFlip <= 100 && choice.toLowerCase() === "heads") {
+    if (coinFlip == 1 && choice.toLowerCase() === "heads") {
         document.write("The flip was heads and you chose heads...you win!");
-    } else if (coinFlip <= 100 && choice.toLowerCase() === ("tails")) {
+    } else if (coinFlip == 1 && choice.toLowerCase() === "tails") {
         document.write("The flip was heads and you chose tails...you lose!");
-    } else if (coinFlip >= 100 && choice.toLowerCase() === ("heads")) {
+    } else if (coinFlip == 0 && choice.toLowerCase() === "heads") {
         document.write("The flip was tails and you chose heads...you lose!");
-    } else if (coinFlip >= 100 && choice.toLowerCase() === ("tails")) {
+    } else if (coinFlip == 0 && choice.toLowerCase() === "tails") {
         document.write("The flip was tails and you chose tails...you win!");
     } else {
             randomNum = coinFlip
@@ -16,5 +16,6 @@ function whatSide(coinFlip,choice) {
 
 let randomNum = Math.round(Math.random());
 let userInput = prompt("Heads or Tails?");
+console.log(randomNum)
 
 whatSide(randomNum,userInput)
